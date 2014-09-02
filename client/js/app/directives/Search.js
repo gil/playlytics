99b92f2d8cdb2046041e51887085b55f767e6fba
André Gil <contato@andregil.org>
2014-09-01 22:48:22 -0300

    search progress and styles

--- client/js/app/directives/Search.js
+++ client/js/app/directives/Search.js
@@ -11,6 +11,8 @@ angular.module("playlytics")
       link: function(scope, element) {
 
         var searchInput = element.find(".search-input");
+        var spinnerSpan = element.find(".spinner")[0];
+        var spinner = null;
         var searchTimeout = null;
 
         function searchAfterDelay() {
@@ -18,9 +20,15 @@ angular.module("playlytics")
           var searchFilter = searchInput.val();
 
           if( searchFilter.length > 0 ) {
-            SpotifyService.search( searchFilter ).success(function(data) {
-              scope.tracks = SpotifyService.parseTracks( data.tracks.items );
-            });
+
+            spinner = new Spinner({lines: 8, length: 4, width: 4, radius: 5, speed: 2, color: "#FFF"}).spin(spinnerSpan);
+
+            SpotifyService.search( searchFilter )
+              .success(function(data) {
+                scope.tracks = SpotifyService.parseTracks( data.tracks.items );
+                spinner.stop();
+              })
+              .error(function() { spinner.stop(); });
           }
         }
 
--- client/js/app/service/SpotifyService.js
+++ client/js/app/service/SpotifyService.js
@@ -1,3 +1,4 @@
+/* jshint camelcase: false */
 "use strict";
 
 angular.module("playlytics")
@@ -5,13 +6,14 @@ angular.module("playlytics")
     return {
 
       parseTracks: function(tracks) {
-        console.log( tracks );
         return _.map(tracks, function(track) {
+
+          // console.log( track );
 
           return {
             name : track.name,
             artist : ( track.artists && track.artists.length > 0 ? track.artists[0].name : "N/A" ),
-            albumCover : ( track.album.images && track.album.images.length > 0 ? track.album.images[0].url : null ),
+            albumCover : ( track.album && track.album.images && track.album.images.length > 0 ? track.album.images[0].url : null ),
             duration : track.duration_ms,
             popularity : track.popularity
           };
